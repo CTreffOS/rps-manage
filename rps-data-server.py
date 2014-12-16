ad6fda69f8c213261f4614fe002bbd783fd4c846
Sven Haardiek <Sven@Haardiek.de>
2014-12-17 00:53:05 +0100

    Worked on REST point and output
    - Deleted unnecessary REST point
    - Changed output so that it matches the needs and fits to the new database
      schema.
    
    Signed-off-by: Sven Haardiek <Sven@Haardiek.de>

--- rps-data-server.py
+++ rps-data-server.py
@@ -20,70 +20,35 @@ def teardown_request(exception):
 		db.close()
 
 
-@app.route('/user')
-def show_user():
-	user = g.db.execute('select user_id, name, email, played, highscore from '
-			'user').fetchall()
-	result = []
-	for u in user:
-		result.append({'user_id' : u[0], 'name' : u[1], 'email' : u[2],
-				'played' : u[3], 'highscore' : u[4]})
-	return jsonify({'player' : result}), 200
-
-
-@app.route('/user/code/<code>')
-def show_user_by_code(code):
-	user = g.db.execute('select user_id, name, email, played, highscore from '
-			'user where code=?', (code,)).fetchone()
-	if not user:
-		return 'User not found', 404
-	result = {'user_id' : user[0], 'name' : user[1], 'email' : user[2], 'played'
-			: user[3], 'highscore' : user[4]}
-	return jsonify(result), 200
-
-
-@app.route('/user/id/<int:id>')
-def show_user_by_id(id):
-	user = g.db.execute('select user_id, name, email, played, highscore from '
-			'user where user_id=?', (id,)).fetchone()
-	if not user:
-		return 'User not found', 404
-	result = {'user_id' : user[0], 'name' : user[1], 'email' : user[2], 'played'
-			: user[3], 'highscore' : user[4]}
-	return jsonify(result), 200
-
-
-@app.route('/game')
-def show_games():
-	games = g.db.execute('select game_id, user_id_1, user_id_2, win_player_1, win_player_2 '
-			'from game').fetchall()
-	result = []
-	for ga in games:
-		result.append({'game_id' : ga[0], 'user_id_1' : ga[1], 'user_id_2' : ga[2], 
-			'won_1' : ga[3], 'won_2' : ga[4]})
-	return jsonify({'games' : result}), 200
-
-
 @app.route('/lastgame')
 def show_last_game():
-	ga = g.db.execute('select game_id, user_id_1, user_id_2, win_player_1, win_player_2 '
-			'from game order by game_id desc limit 0,1').fetchone()
+	ga = g.db.execute('select user_id_1, user_id_2, win_player_1, win_player_2, '
+			'rock_player_1, paper_player_1, scissors_player_1, rock_player_2, '
+			'paper_player_2, scissors_player_2 ' 'from game order by game_id desc '
+			'limit 0,1').fetchone()
 	if not ga:
 		return 'Last game not found', 404
-	result = {'game_id' : ga[0], 'user_id_1' : ga[1], 'user_id_2' : ga[2], 
-		'won_1' : ga[3], 'won_2' : ga[4]}
+	player1 = g.db.execute('select name from user where user_id = ?', (ga[0],)).fetchone()
+	player2 = g.db.execute('select name from user where user_id = ?', (ga[1],)).fetchone()
+	if not (player1 or player2):
+		return 'Player not found', 500
+	result = {'user_1' : player1[0], 'user_2' : player2[0], 'won_1' : ga[2],
+			'won_2' : ga[3], 'rock_1' : ga[4], 'paper_1' : ga[5],'scissors_1' :
+			ga[6], 'rock_2' : ga[7], 'paper_2' : ga[8],'scissors_2' : ga[9]}
 	return jsonify(result), 200
 
 
 @app.route('/highscore')
 def show_highscore():
-	user = g.db.execute('select user_id, name, email, played, highscore from '
-			'user where highscore not null').fetchall()
+	user = g.db.execute('select user_id, name, highscore from user where highscore not null').fetchall()
 	result = {}
 	for u in user:
-		result[u[4]] = {'user_id' : u[0], 'name' : u[1], 'email' : u[2],
-				'played' : u[3]}
+		won = g.db.execute('select count() from game where (user_id_1 = ? and '
+				'win_player_1 > win_player_2) or (user_id_2 = ? and win_player_2 > '
+				'win_player_1)', (u[0],u[0])).fetchone()[0]
+		result[u[2]] =	{'user_id' : u[0], 'name' : u[1], 'won' : won}
 	return jsonify(result), 200
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True, threaded=True)
